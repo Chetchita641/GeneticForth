3eb89e1483bcebe129e5d3783dc768bf72a5d9de
Chris Macholtz <cmacholtz@jogusa-chris.joganic.com>
2018-06-06 16:06:15 -0700

    intermediate commit

--- genetic.cpp
+++ genetic.cpp
@@ -2,301 +2,280 @@
 // genetic.cpp
 // vim = ts=8 sts=8 sw=4 noet
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <cstdlib>
-#include <ctime>
-#include <string.h>
 #include <iostream>
-#include <sstream>
+#include <string>
+#include <ctime>
+#include <cmath>
+#include <cstdlib>
+
+#define M_LENGTH 10 
+#define ITERATIONS 500
+#define M_NUM 10
+#define CO_RANGE 100
+
+//Functions
+char* NewMethods();
+int* NewCoefficients();
+int Calculate(char* method, int* coefficients, int &a, int &b);
+float ValidateSet(char* method, int* coefficients, int result);
+int RandomChoice(float weightedAccuracy[]);
+char* MakeChildMethod(char* fatherMethod, char* motherMethod);
+int* MakeChildCoefficients(int* fatherCoefficient, int* motherCoefficient);
 
 using namespace std;
 
-const int mLen = 10;
-const int randRange = 100;
-const int numMethods = 10;
-const int iterations = 500;
-
-void InitMethods();
-int Calculate(char* method, int a, int b);
-string ForthTranslation(char* method, int a, int b);
-float ValidSet(char* method, int result);
-int RandomChoice(float sigmoids[10]);
-void MakeChild(int father, int mother);
-
-char methods[numMethods][mLen + 1];
-char newMethods[numMethods][mLen + 1];
-static int Coefficients[mLen];
-int aPlace = -1;
-int bPlace = -1;
-
-const bool forthTranslation = false;
-
 int main(int argc, char** argv)
 {
+	srand (time(NULL));
 	bool debug = false;
 	int i;
 
 	int a;
 	int b;
 	int result;
-	float accuracy[numMethods];
-	float accuracyTotal;
-	float sigmoids[numMethods];
+	int aPlace;
+	int bPlace;
+
+	float validAccuracy[M_NUM];
+	float rawAcc = 0;
+	float weightedAccuracy[M_NUM];
+
 	int father;
 	int mother;
+	char* fatherMethod;
+	char* motherMethod;
+	char childMethod[M_LENGTH+1];
+	int childCoefficients[M_LENGTH+1];
+
+	char* methodsArray[M_NUM];
+	int* coefficientsArray[M_NUM];
+
+	//Draw from generated methods and calcuate their values. Then, validate.
+	cout << "Please enter in a value for a: ";
+	cin >> a;
+
+	cout << "Please enter in a value for b: ";
+	cin >> b;
+
+	cout << "Method key:\n" <<
+			"a) Add\n" <<
+			"b) Subtract\n" <<
+			"c) Multiply\n" <<
+			"d) Divide\n" <<
+			"e) Modulo\n" <<
+			"f) Halt\n";
 	
-	InitMethods();
-	while (a != -1)
+	for (i = 0; i < M_NUM; ++i)
 	{
-		// Set seed for random number generator
-		srand( rand() ^ time(NULL) );
-
-		
-		printf ("Please enter a value for a: ");
-		scanf ("%d", &a);
-		if (a == -1)
-			break;
-
-		printf ("Please enter a value for b: ");
-		scanf ("%d", &b);
-
-		printf ("Method key:\n"
-			"a) Add\n"
-			"b) Subtract\n"
-			"c) Multiply\n"
-			"d) Divide\n"
-			"e) Modulo\n"
-			"f) Swap\n"
-			"g) Duplicate\n"
-			"h) Over\n" 
-			"i) Rotate\n");
-
-		for (int iter = 0; iter < iterations; ++iter)
-		{
-			accuracyTotal = 0.0;
-			for (i = 0; i < numMethods; ++i)
-			{
-				// Draw from generated methods (sequence of operations)
-				if (debug) printf("Method[%d]: %s\n", i, methods[i]);
-				aPlace = -1;
-				bPlace = -1;
-				result = Calculate(methods[i], a, b);
-			
-				if (forthTranslation) 
-				{
-				  string trans = ForthTranslation(methods[i], a, b);
-				  cout << trans;
-				}
-
-				// Determine their accuracy compared to validation set
-				accuracy[i] = ValidSet(methods[i], result);
-				accuracyTotal += accuracy[i];
-			}
-			if (debug) printf("\n");
-			
-			// Convert methods into sigmoids
-			for (i = 0; i < numMethods; ++i)
-			{
-				sigmoids[i] = accuracy[i] / accuracyTotal;
-				if (debug) printf("Sigmoids[%d]: %f\n", i, sigmoids[i]);
-			}
-
-			// Create ten children randomly using the weights
-			for (i = 0; i < numMethods; ++i)
-			{
-				father = RandomChoice(sigmoids);
-				mother = RandomChoice(sigmoids);
-				MakeChild(father, mother);
-			}
-			if (debug) printf ("\n");
-			
-		}
+		methodsArray[i] = NewMethods();
+		coefficientsArray[i] = NewCoefficients();
 	}
-	return 0;
-}
 
-void InitMethods()
+	for (i = 0; i < M_NUM; ++i)
+	{
+		cout << "Method#" << i << ": " << methodsArray[i] << "\n";
+		result = Calculate(methodsArray[i], coefficientsArray[i], a, b);
+
+		cout << "\nValidation Set\n";
+		validAccuracy[i] = ValidateSet(methodsArray[i], coefficientsArray[i], result);
+		rawAcc += validAccuracy[i];
+	}
+
+	//Create a weighted accuracy to rank methods
+	for (i = 0; i < M_NUM; ++i)
+	{
+		weightedAccuracy[i] = validAccuracy[i] / rawAcc;
+		cout << "Weighted Accuracy for method #" << i << ": " << weightedAccuracy[i] << "\n";
+	}
+	
+	//Make arrays of children and then replace the old arrays
+	
+	for (i = 0; i < M_NUM; ++i)
+	{
+		father = RandomChoice(weightedAccuracy);
+		mother = RandomChoice(weightedAccuracy);
+	
+		cout << "Father method: " << methodsArray[father] << "\n";
+		cout << "Mother method: " << methodsArray[mother] << "\n\n";
+
+		fatherMethod = methodsArray[father];
+		motherMethod = methodsArray[mother];
+
+		childMethod[i] = MakeChildMethod(fatherMethod, motherMethod);
+
+		cout << "Child method: " << childMethod[i] << "\n\n";
+	}
+
+	return 0;
+}	
+
+char* NewMethods()
 {
 	int r;
-	char method[mLen+1];
+	int i;
 
-	for (int i = 0; i < numMethods; ++i)
+	char* method = new char[M_LENGTH+1];
+
+	for (i = 0; i < M_LENGTH; ++i)
 	{
-		for (int j = 0; j < mLen; ++j)
-		{
-			r = rand() % 6;
-			methods[i][j] = 'a' + r;
-		}
-	}	
+		r = rand() % 6;
+		method[i] = 'a' + r;
+	}
+	method[i] = '\0';
+	return method;
 }
 
-int Calculate(char* method, int a, int b)
+int* NewCoefficients()
+{
+	int* coefficients = new int[M_LENGTH+1];
+	int aPlace;
+	int bPlace;
+
+	aPlace = rand() % M_LENGTH;
+	bPlace = rand() % M_LENGTH;
+	while (aPlace == bPlace)
+		bPlace = rand() % M_LENGTH;
+
+	for (int i = 0; i < M_LENGTH+1; ++i)
+	{
+		if (i == aPlace)
+			coefficients[i] = CO_RANGE + 1;
+		else if (i == bPlace)
+			coefficients[i] = CO_RANGE + 2;
+		else
+			coefficients[i] = rand() % CO_RANGE;
+	}
+
+	return coefficients;
+}
+
+int Calculate(char* method, int* coefficients, int &a, int &b)
 {
 	bool debug = false;
 
-	int result = 0;
+	int result = *coefficients;
 	int prev;
-	int coEff;
+	int c;
 
-	int len = (int) strlen(method);
+
+	//Insert the first number
+	if (*coefficients == CO_RANGE + 1)
+		c = a;
+	else if (*coefficients == CO_RANGE + 2)
+		c = b;
+	else
+		c = *coefficients;
 	
-	if (aPlace == -1)
-	  aPlace = rand() % len;
-	if (bPlace == -1)
-	  bPlace = aPlace;
-	while (bPlace == aPlace)
-	{
-		bPlace = rand() % len;
-	}
+	result = c;
+	coefficients++;
 
-	if (aPlace == 0)
-		result = a;
-	else if (bPlace == 0)
-		result = b;
-	else 
-		result = rand () % randRange;
-	for (int i = 1; i < len; ++i)
+	for (int i = 0; i < M_LENGTH; ++i)
 	{
-		if (i == aPlace) {
-			coEff = a;
-		} else if (i == bPlace) {
-			coEff = b;
-		} else {
-			coEff = rand() % randRange;
+		if (*coefficients == CO_RANGE + 1)
+			c = a;
+		else if (*coefficients == CO_RANGE + 2)
+			c = b;
+		else
+			c = *coefficients;
+		
+		if (*method == 'a')
+		{
+			prev = result;
+			result += c;
+			if (debug) cout << prev << " + " << c << " = " << result << "\n";
 		}
-
-		if (method[i-1] == 'a') {
-			prev = result;	
-			result += coEff;
-			Coefficients[i-1] = coEff;
-			if (debug) printf ("%d + %d = %d\n", prev, coEff, result);
+		else if (*method == 'b')
+		{
+			prev = result;
+			result -= c;
+			if (debug) cout << prev << " - " << c << " = " << result << "\n";
 		}
-		else if (method[i-1] == 'b') {
-			prev = result;	
-			result -= coEff;
-			Coefficients[i-1] = coEff;
-			if (debug) printf ("%d - %d = %d\n", prev, coEff, result);
+		else if (*method == 'c')
+		{
+			prev = result;
+			result *= c;
+			if (debug) cout << prev << " * " << c << " = " << result << "\n";
 		}
-		else if (method[i-1] == 'c') {
-			prev = result;	
-			result *= coEff;
-			Coefficients[i-1] = coEff;
-			if (debug) printf ("%d * %d = %d\n", prev, coEff, result);
-		}
-		else if (method[i-1] == 'd') {
-			if (coEff == 0)
+		else if (*method == 'd')
+		{
+			if (c == 0)
+				cout << "Cannot divide by zero\n";
+			else
 			{
-				printf ("Cannot divide by zero\n");
-				continue;
-			}	
-			prev = result;	
-			result /= coEff;
-			Coefficients[i-1] = coEff;
-			if (debug) printf ("%d / %d = %d\n", prev, coEff, result);
+				prev = result;
+				result /= c;
+				if (debug) cout << prev << " / " << c << " = " << result << "\n";
+			}
 		}
-		else if (method[i-1] == 'e') {
-			if (coEff == 0)
+		else if (*method == 'e')
+		{
+			if (c == 0)
+				cout << "Cannot divide by zero\n";
+			else
 			{
-				printf ("Cannot divide by zero\n");
-				continue;
-			}	
-			prev = result;	
-			result %= coEff;
-			Coefficients[i-1] = coEff;
-			if (debug) printf ("%d %% %d = %d\n", prev, coEff, result);
+				prev = result;
+				result %= c;
+				if (debug) cout << prev << " % " << c << " = " << result << "\n";
+			}
 		}
-		else if (method[i-1] == 'f')
+		else if (*method == 'f')
 		{
 			if (i == 0)
-				printf ("%d", result);
+				cout << result << "\n";
 			break;
 		}
+
+		method++;
+		coefficients++;
 	}
+
+	cout << "Calculated result: " << result << "\n";
 	return result;
+
 }
 
-string ForthTranslation(char* method, int a, int b)
+float ValidateSet(char* method, int* coefficients, int result)
 {
-	stringstream sstm;
-	sstm << "sf " << a << " ";
-	int len = (int) strlen(method);
-	for (int i = 0; i < len; ++i)
+	int validResult;
+	int diff = 0;
+	float accuracy;
+
+	int validA[10] = { 5, 13, 8, 33, 7, 3, 82, 4, 37, 0 };
+	int validB[10] = { 10, -2, 8, 2, 42, -8, -40, -18, 0, 0 };
+	int validY[10] = { 60, 128, 88, 332, 112, 22, 780, 22, 370, 0 };
+
+	for (int i = 0; i < M_NUM; ++i)
 	{
-		if (method[i] == 'a')  sstm << Coefficients[i] << " + "; 
-		else if (method[i] == 'b')  sstm << Coefficients[i] << " - ";
-		else if (method[i] == 'c')  sstm << Coefficients[i] << " * ";
-		else if (method[i] == 'd')  sstm << Coefficients[i] << " / ";
-		else if (method[i] == 'e')  sstm << Coefficients[i] << " /MOD ";
+		validResult = Calculate(method, coefficients, validA[i], validB[i]);
+		diff += abs(validResult - validY[i]);
 	}
 
-	sstm << ". bye\n";
-	string tmp = sstm.str();
-	return tmp.c_str();
+	cout << "Total difference from validation: " << diff << "\n\n";
+	accuracy = 1.0 / (float) diff;
+	return accuracy;
 }
 
-//Tests for y = a * 10 + b
-float ValidSet(char* method, int result)
+int RandomChoice(float weightedAccuracy[])
 {
-	bool debug = true;
-
-	int testA[10] = { 5, 13, 8, 33, 7, 3, 82, 4, 37, 0 };
-	int testB[10] = { 10, -2, 8, 2, 42, -8, -40, -18, 0, 0 };
-	int testY[10] = { 60, 128, 88, 332, 112, 22, 780, 22, 370, 0 };
-
-	int testResult;
-	int resultTotal = 0;
-	float averageError;
-	int difference;
-	for (int i = 0; i < 10; ++i)
-	{
-		testResult = Calculate(method, testA[i], testB[i]);
-		difference = abs(testResult - testY[i]);
-		resultTotal += difference;
-		if (debug) printf ("Difference from Test Set %i: %i\n", i, difference);
-	}
-	averageError = (float) resultTotal / 10.0;
-	if (debug) printf ("Method: %s\n", method);
-	if (debug) printf ("Average error: %f\n\n", averageError);
-	return 1 / averageError;
-}
-
-int RandomChoice(float sigmoids[10])
-{
-	bool debug = false;
 	int r;
 	float thresh = 1.0;
-	while (thresh > 0.0)
-	{
-		r = rand() % numMethods;
-		if (debug) printf ("Index: %d\n", r);
-		if (thresh < sigmoids[r])
-		{
-			if (debug) printf ("\nReturning %d\n", r);
-			return r;
-		}
-		thresh -= sigmoids[r];
-	}
+	r = rand() % M_NUM;
 
-	return -1;
+	while (thresh > weightedAccuracy[r])
+	{
+		thresh -= weightedAccuracy[r];
+		r = rand() % M_NUM;
+	}
+	return r;
 }
 
-void MakeChild(int father, int mother)
+char* MakeChildMethod(char* fatherMethod, char* motherMethod)
 {
-	int i;
-	int j;
-	int median = mLen / 2;
+	return fatherMethod;
+}
 
-	for (i = 0; i < numMethods; ++i)
-	{
-		for (j = 0; j < median-1; ++j)
-		{
-			methods[i][j] = methods[father][j]; 	
-		}
-		for (i = median; i < mLen; ++i)
-		{
-			methods[i][j] = methods[mother][j];
-		}
-	}
+int* MakeChildCoefficients(int* fatherCoefficient, int* motherCoefficient)
+{
+	return new int[M_LENGTH+1];
 }
 
