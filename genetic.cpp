4dc2fcdbb4596d44ceb0a3af1c9adc362420a128
Chris Macholtz <cmacholtz@jogusa-chris.joganic.com>
2018-06-11 09:03:43 -0700

    updated interface; added Forth translation

--- genetic.cpp
+++ genetic.cpp
@@ -5,9 +5,10 @@
 #include <stdio.h>
 #include <ctime>
 #include <cstdlib>
-#include <string.h>
+#include <string>
+#include <sstream>
 
-#define METHOD_NUMBER 16
+#define METHOD_NUMBER 100
 #define METHOD_LENGTH 10
 #define METHOD_COUNT 6
 #define CO_RANGE 100
@@ -26,6 +27,7 @@ char GetOperation();
 int GetCoefficient();
 int Calculate(Method method, int &a, int &b);
 float Validate(Method method, int result);
+void ShowBest(Method method, float &maxAccuracy, int &a, int &b);
 Method* NextGeneration(Method* oldMethods, float* pSigmoids);
 int ParentChoice(float* pSigmoids);
 
@@ -38,7 +40,9 @@ int main(int argc, char** argv)
     int b;
     int maxIterations;
     int result;
-    float differences[METHOD_NUMBER];
+    float accuracies[METHOD_NUMBER];
+    float maxAccuracy;
+    int maxAccuracyIndex;
     float differenceTotal;
     float sigmoids[METHOD_NUMBER];
     float* pSigmoids = sigmoids;
@@ -46,46 +50,57 @@ int main(int argc, char** argv)
     Method* methods = InitMethods();
     Method* oldMethods = methods;
     
-    printf ("Please enter the number of iterations: ");
-    scanf ("%i", &maxIterations);
-    printf ("Please enter in a value for a: ");
-    scanf ("%i", &a);
-    printf ("Please enter in a value for b: ");
-    scanf ("%i", &b);
-    printf ("Method key:\n"
-	    "a) Add\n" 
-	    "b) Subtract\n" 
-	    "c) Multiply\n" 
-	    "d) Divide\n" 
-	    "e) Modulo\n" 
-	    "f) Halt\n");
-
-    for (int iterations = 0; iterations < maxIterations; ++iterations)
+    while (a != -1)
     {
-	differenceTotal = 0;
-	for (i = 0; i < METHOD_NUMBER; ++i)
-	{
-	    printf ("\nMethod #%i: %s\n", i, methods[i].op);
-	    result = Calculate(methods[i], a, b);
-	    printf ("Result: %i\n", result);
-	    differences[i] = Validate(methods[i], result);
-	    printf ("Total Accuracy from validation set: %f\n", differences[i]);
-	    differenceTotal += differences[i];
-	}
+	printf ("Please enter the number of iterations, or 0 to quit: ");
+	scanf ("%i", &maxIterations);
+	if (maxIterations <= 0)
+	    break;
+	printf ("Please enter in a value for a, or 0 for random: ");
+	scanf ("%i", &a);
+	if (a == 0)
+	    a = rand() % CO_RANGE;
+	printf ("Please enter in a value for b, or 0 for random: ");
+	if (b == 0)
+	    b = rand() % CO_RANGE;
+	scanf ("%i", &b);
+	printf ("Method key:\n"
+		"a) Add\n" 
+		"b) Subtract\n" 
+		"c) Multiply\n" 
+		"d) Divide\n" 
+		"e) Modulo\n" 
+		"f) Halt\n");
 
-	printf ("\n");
-	for (i = 0; i < METHOD_NUMBER; ++i)
+	for (int iterations = 0; iterations < maxIterations; ++iterations)
 	{
-	    sigmoids[i] = differences[i] / differenceTotal;
-	    printf ("Sigmoid #%i: %f\n", i, sigmoids[i]);	
-	}
+	    printf ("\nIteration #%i:\n", iterations);
+	    differenceTotal = 0;
+	    maxAccuracy = 0.0;
+	    for (i = 0; i < METHOD_NUMBER; ++i)
+	    {
+		result = Calculate(methods[i], a, b);
+		accuracies[i] = Validate(methods[i], result);
+		if (accuracies[i] > maxAccuracy)
+		{
+		    maxAccuracy = accuracies[i];
+		    maxAccuracyIndex = i;
+		}
+		differenceTotal += accuracies[i];
+	    }
 
-	printf ("\n");
-	oldMethods = methods;
-	methods = NextGeneration(oldMethods, pSigmoids);
-	delete oldMethods;
+	    ShowBest(methods[maxAccuracyIndex], maxAccuracy, a, b);
+
+	    for (i = 0; i < METHOD_NUMBER; ++i)
+	    {
+		sigmoids[i] = accuracies[i] / differenceTotal;
+	    }
+
+	    oldMethods = methods;
+	    methods = NextGeneration(oldMethods, pSigmoids);
+	    delete oldMethods;
+	}
     }
-
     return 0;
 }
 
@@ -178,6 +193,46 @@ float Validate(Method method, int result)
     
     return 1 / difference;
 }
+
+void ShowBest(Method method, float &maxAccuracy, int &a, int &b)
+{
+    stringstream sstm;
+    char op;
+    int co;
+
+    sstm << "sf " << method.co[0] << " ";
+    for (int i = 1; i < METHOD_LENGTH; ++i)
+    {
+	if (method.op[i] == '-')
+	    continue;
+
+	if (method.co[i] == CO_RANGE + 1)
+	    co = a;
+	else if (method.co[i] == CO_RANGE + 2)
+	    co = b;
+	else
+	    co = method.co[i];
+
+	if (method.op[i] == 'a')
+	    op = '+';
+	else if (method.op[i] == 'b')
+	    op = '-';
+	else if (method.op[i] == 'c')
+	    op = '*';
+	else if (method.op[i] == 'd')
+	    op = '/';
+	else if (method.op[i] == 'e')
+	    op = '%';
+	sstm << co << " " << op << " ";
+    }
+    sstm << ". bye";
+
+    printf ("Best Method: %s\n", method.op);
+    printf ("Accuracy: %f", maxAccuracy);
+    printf ("\n%s\n", sstm.str().c_str());
+    return;
+}
+
 
 Method* NextGeneration(Method* oldMethods, float* pSigmoids)
 {
